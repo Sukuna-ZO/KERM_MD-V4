3a6b5f5a57fae777dd49545b34c5cce6f3c968cd
Sukuna-ZO <smenace30@gmail.com>
2024-12-17 11:40:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "+237699586756",
+SESSION_ID: process.env.SESSION_ID || "qiIgXaxQ#xTAR3niMooVxpO5LQnuOUhIeqwJreaT1VtJxX_LvzFA",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
